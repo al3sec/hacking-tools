294edceb4f7dd9cf21893d234c4f4a7bc7ed3e74
alejandro <148832436+al3sec@users.noreply.github.com>
2024-03-27 14:15:13 -0300

    Update web-recon.sh
    adding crt.sh subdomain results

--- recon/web-recon.sh
+++ recon/web-recon.sh
@@ -14,7 +14,10 @@ echo "starting knockpy ..."
 python3 ~/Desktop/sideprojects/Hackthebox/tools/knock/knockpy.py $1 -o .
 cat *.json  | jq 'keys[]' > "$domain-knockpy-output.txt"; sed -i -e 's/"//g'  -e '1d' "$domain-knockpy-output.txt"
 
-cat "$domain-amass-output.txt" "$domain-subfinder-output.txt" "$domain-knockpy-output.txt"  > "$domain-subdomains.txt"
+echo "consulting crt.sh in order to fetch subdomains ..."
+curl -s "https://crt.sh/?q=$1&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | tee "$domain-crtsh-output.txt"
+
+cat "$domain-amass-output.txt" "$domain-subfinder-output.txt" "$domain-knockpy-output.txt"  "$domain-crtsh-output.txt" > "$domain-subdomains.txt"
 
 # Just unique subdomains
 sort "$domain-subdomains.txt" | uniq > "$domain-uniq-subdomains.txt"; rm "$domain-subdomains.txt"
@@ -33,7 +36,7 @@ sort "$domain-subdomains.txt" | uniq > "$domain-uniq-subdomains.txt"
 
 # Now, it is the time to do DNS resolution.
 echo "checking with dns resolution ..."
-cat "$domain-uniq-subdomains.txt" | puredns resolve --resolvers /opt/useful/resolvers.txt --write "$domain-subdomains-perm.txt"
+cat "$domain-uniq-subdomains.txt" | puredns resolve --resolvers /opt/useful/resolvers.txt --write "$domain-subdomains-perm.txt" --rate-limit 500
 
 # Checking host command to see more details
 echo "checking subdomains with host command ..."
@@ -43,13 +46,18 @@ cat "$domain-subdomains-perm.txt" | xargs -I{} host {} | tee -a "$domain-host-ou
 echo "testing http services ..."
 cat "$domain-subdomains-perm.txt" | httprobe -p http:81 -p http:900 -p https:900 -p http:3000 -p https:3000 -p http:3001 -p https:3001 -p http:5000 -p https:5000 -p http:7070 -p https:7070 -p http:8000 -p https:8000 -p http:8008 -p https:8008 -p http:8080 -p https:8080 -p http:8443 -p https:8443 -p http:9000 -p https:9000 -p http:9090 -p https:9090 -c 50 | tee "$domain-online-subdomains.txt"
 
-echo "starting ffuf ..."
+echo "starting ffuf to bruteforce subdomains..."
 ffuf -w /opt/useful/SecLists/Discovery/DNS/subdomains-top1million-110000.txt -u "https://FUZZ.$1" -o "$domain-json-ffuf-output.txt"
 cat "$domain-json-ffuf-output.txt" | jq '.results[].url' > "$domain-ffuf-output.txt"
 sed -i -e 's/"//g' "$domain-ffuf-output.txt"
 
+echo "starting ffuf to bruteforce vhosts..."
+ffuf -w /opt/useful/SecLists/Discovery/DNS/subdomains-top1million-110000.txt -u "https://$1" -H "Host: FUZZ.$1" -o "$domain-vhosts-json-ffuf-output.txt" -mc 200,204,301,302,307
+cat "$domain-vhosts-json-ffuf-output.txt" | jq '.results[].url' > "$domain-ffuf-vhosts-output.txt"
+sed -i -e 's/"//g' "$domain-ffuf-vhosts-output.txt"
+
 # Adding ffuf output to online subdoms
-cat "$domain-ffuf-output.txt" >> "$domain-online-subdomains.txt"
+cat "$domain-ffuf-output.txt" "$domain-ffuf-vhosts-output.txt" >> "$domain-online-subdomains.txt"
 
 # Just unique http endpoints
 sort "$domain-online-subdomains.txt" | uniq > "$domain-final-uniq-online-subdomains.txt"
@@ -57,15 +65,14 @@ sort "$domain-online-subdomains.txt" | uniq > "$domain-final-uniq-online-subdoma
 
 # From there, visual inspection is a good idea, aquatone is a great tool, however most people
 # don’t realise it will also accept endpoints and files
-# echo "starting aquatone ..."
-# cat "$domain-final-uniq-online-subdomains.txt" | aquatone
+echo "starting aquatone ..."
+cat "$domain-final-uniq-online-subdomains.txt" | aquatone
 
 # Using a crawler
 # cat final-uniq-online-subdomains.txt | sudo docker run --rm -i hakluke/hakrawler -d 3 > hakrawler-output.txt
 
 # Using nmap in order to see more services on these subdomains:
 # nmap -p- --min-rate=800 -iL subdomains-perm.txt -oN nmap-report.txt
-# rm subdomains-perm.txt
 
 # Using nuclei to going deeper
 # nuclei -l final-uniq-online-subdomains.txt -o nuclei-output.txt
